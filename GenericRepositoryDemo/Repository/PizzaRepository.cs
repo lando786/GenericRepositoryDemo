dcdbf01a16fcaff063a88ee3ac5f2772e78830b1
Luis Dasta <luis.dasta@growersedge.com>
2021-12-14 10:56:39 -0600

    demo start point

--- GenericRepositoryDemo/Repository/PizzaRepository.cs
+++ GenericRepositoryDemo/Repository/PizzaRepository.cs
@@ -1,22 +1,37 @@
 ﻿using GenericRepositoryDemo.Data;
 using GenericRepositoryDemo.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace GenericRepositoryDemo.Repository
 {
-    public interface IPizzaRepository : IGenericRepository<Pizza>
+    public interface IPizzaRepository// : IGenericRepository<Pizza>
     {
-       
+        Task<Pizza> Add(Pizza pizza);
+
+        Task<IList<Pizza>> GetAll();
     }
-    public class PizzaRepository : GenericRepository<Pizza>, IPizzaRepository
+
+    public class PizzaRepository : IPizzaRepository // GenericRepository<Pizza>, IPizzaRepository
     {
-       
-        public PizzaRepository(IPizzaContextFactory pizzaContextFactory) : base(pizzaContextFactory)
+        private PizzaContext _context;
+        private DbSet<Pizza> table = null;
+
+        public PizzaRepository(IPizzaContextFactory pizzaContextFactory)// : base(pizzaContextFactory)
         {
+            _context = pizzaContextFactory.CreateContext();
+            table = _context.Set<Pizza>();
         }
 
-        public void SpecialPizzaMethod()
+        public async Task<Pizza> Add(Pizza pizza)
         {
+            var newpizza = await table.AddAsync(pizza);
+            await _context.SaveChangesAsync();
+            return newpizza.Entity;
+        }
+
+        public async Task<IList<Pizza>> GetAll()
+        {
+            return await table.ToListAsync();
         }
     }
 }
